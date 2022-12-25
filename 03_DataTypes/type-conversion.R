7cecc852b9bfb887ce1950e294f4b44d5ca93a3b
luhuadong <luhuadong@163.com>
2022-12-25 18:12:20 +0800

    add data type conversion example

---
+++ 03_DataTypes/type-conversion.R
@@ -0,0 +1,16 @@
+x <- 1L # integer
+y <- 2 # numeric
+
+# convert from integer to numeric:
+a <- as.numeric(x)
+
+# convert from numeric to integer:
+b <- as.integer(y)
+
+# print values of x and y
+x
+y
+
+# print the class name of a and b
+class(a)
+class(b)
