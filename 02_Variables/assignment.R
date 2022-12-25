7fa11bc44130d9367ccae3055ec8682c8909112a
luhuadong <luhuadong@163.com>
2022-12-25 18:11:55 +0800

    add variable assignment and concatenate example

---
+++ 02_Variables/assignment.R
@@ -0,0 +1,12 @@
+# 使用等号 = 号赋值
+var.1 = c(0,1,2,3)          
+print(var.1)
+
+# 使用左箭头 <-赋值
+var.2 <- c("learn","R")  
+print(var.2)
+   
+# 使用右箭头 -> 赋值
+c(TRUE,1) -> var.3
+print(var.3)
+ 
---
+++ 02_Variables/concatenate.R
@@ -0,0 +1,4 @@
+text1 <- "R is"
+text2 <- "awesome"
+
+paste(text1, text2)
